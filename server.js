38e320015bed639228af75e59aa2acaad30f7d9b
TiGuy <dracine77@gmail.com>
2016-09-14 16:05:16 -0400

    .

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ require('beepboop-slapp-presence-polyfill')(slapp, { debug: true })
 var app = slapp.attachToExpress(express())
 
 slapp.message('CreateRequest (.*)', 'direct_message', (msg, text, match1) => {
-  slapp.client.users.info(msg.meta.bot_token, msg.body.event.user, (err, data) => {
+  slapp.api.users.info(msg.meta.bot_token, msg.body.event.user, function(err, data) => {
     msg.say({
       text: 'Are you sure you want to create this Request?',
       attachments: [
