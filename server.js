fb6877857e0cd6962ea06f4749b26e5b0c1f8407
TiGuy <dracine77@gmail.com>
2016-09-13 20:11:40 -0400

    adding a first reply test

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ require('beepboop-slapp-presence-polyfill')(slapp, { debug: true })
 require('./flows')(slapp)
 var app = slapp.attachToExpress(express())
 
+slapp.message('hi (.*)', ['direct_message'], (msg, text, match1) => {
+  msg.say('How are you?').route('handleCreateRequest', { what: match1 })
+})
+
+slapp.route('handleCreateRequest', (msg, state) => {
+  msg.say(':smile' + state.what)
+})
 app.get('/', function (req, res) {
   res.send('Hello')
 })
