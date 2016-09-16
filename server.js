a35ef57dca68e1e5b319a3fb0e4f14940d8d174c
TiGuy <dracine77@gmail.com>
2016-09-16 11:17:55 -0400

    .

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ slapp.action('CreateRequest_callback', 'answer', (msg, value) => {
       ]
     })
   }
-  else {msg.respond(msg.body.response_url, original_message)}
+  else {msg.respond(text: value)}
 })
 
 app.get('/', function (req, res) {
